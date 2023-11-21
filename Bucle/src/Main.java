0ce199afa419309a6293a7cd5e15c352470f2634
Fermin <fosinagalo@educacion.navarra.es>
2023-11-21 19:44:13 +0100

    Bucle

---
+++ Bucle/src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+            funcion1();
+    }
+
+    public static void funcion1(){
+        for (int i = 0; i<6;i++){
+            if (i%2 == 0){
+                System.out.println("*");
+            }else{
+                System.out.println(".");
+            }
+          //aqui esta el comentario
+
+
+        }
+
+
+    }
+}
