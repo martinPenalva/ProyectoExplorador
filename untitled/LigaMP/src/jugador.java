d4e6a080c5a778c3439a47649833dad9f0b26539
mpenalvart1@educacion.navarra.es <mpenalvart1@educacion.navarra.es>
2024-01-12 18:33:03 +0100

    clase jugador terminada

--- untitled/LigaMP/src/jugador.java
+++ untitled/LigaMP/src/jugador.java
@@ -1,1 +1,51 @@
 
+import java.util.Objects;
+
+public class Jugador {
+
+    private String nombre;
+    private String nacionalidad;
+    private int edad;
+    private String posicion;
+    private boolean lesionado;
+
+    public Jugador(String nombJugador, String nacionalidadJu, int edadJug, String posicionJu)
+    {
+        nombre = nombJugador;
+        nacionalidad = nacionalidadJu;
+        edad = edadJug;
+        if (Objects.equals(posicionJu, "POR") || Objects.equals(posicionJu, "DEF") || Objects.equals(posicionJu, "CTC") || Objects.equals(posicionJu, "DEL"))
+        {
+            posicion = posicionJu;
+        }
+        lesionado = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public boolean getLesionado() {
+        return lesionado;
+    }
+
+    public void Lesionarse()
+    {
+        lesionado = true;
+    }
+    public void Recuperarse()
+    {
+        lesionado = false;
+    }
+}
