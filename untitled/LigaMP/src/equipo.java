5a6b50a8d4673ede28bb69184dd318d8f2dd3cd2
mpenalvart1@educacion.navarra.es <mpenalvart1@educacion.navarra.es>
2024-01-12 18:38:12 +0100

    terminando la clase equipo

--- untitled/LigaMP/src/equipo.java
+++ untitled/LigaMP/src/equipo.java
@@ -17,3 +17,45 @@ public class Equipo {
     public String getNombre() {
         return nombre;
     }
+    public String getCiudad() {
+        return ciudad;
+    }
+    public int getNumJugadores()
+    {
+        int cuantos = 0;
+        for(int posicion = 0;posicion< ListaJugadores.length;posicion++)
+        {
+            if (ListaJugadores[posicion] != null)
+            {
+                cuantos ++;
+            }
+        }
+        return cuantos;
+    }
+    public void mostrarListaJugadores()
+    {
+        for (int posicion = 0;posicion < ListaJugadores.length;posicion++)
+        {
+            if (ListaJugadores[posicion] != null) {
+                String situacion = "";
+                if (ListaJugadores[posicion].getLesionado()) {
+                    situacion = "SI";
+                } else {
+                    situacion = "NO";
+                }
+                System.out.println(ListaJugadores[posicion].getNombre() + "\t\t" + ListaJugadores[posicion].getPosicion() + "\t\t\t\t" + ListaJugadores[posicion].getEdad() + "\t\t" + ListaJugadores[posicion].getNacionalidad() + "\t\t" + situacion);
+            }
+        }
+    }
+    public void adquirirJugador(Jugador jugador)
+    {
+        for (int posicion = 0;posicion < ListaJugadores.length;posicion++)
+        {
+            if(ListaJugadores[posicion] == null)
+            {
+                ListaJugadores[posicion] = jugador;
+                break;
+            }
+        }
+    }
+
