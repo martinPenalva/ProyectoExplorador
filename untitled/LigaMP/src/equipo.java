c63219662504632fbab6cbf369e8fd6547c23841
mpenalvart1@educacion.navarra.es <mpenalvart1@educacion.navarra.es>
2024-01-12 18:36:44 +0100

    empezando con la clase equipo

--- untitled/LigaMP/src/equipo.java
+++ untitled/LigaMP/src/equipo.java
@@ -1,1 +1,19 @@
 
+import java.util.Objects;
+
+public class Equipo {
+    private String nombre;
+    private String ciudad;
+    private final int numMaxJugadores = 22;
+    private Jugador[] ListaJugadores;
+
+    public Equipo(String nombEquipo, String nombCiudad)
+    {
+        nombre = nombEquipo;
+        ciudad = nombCiudad;
+        ListaJugadores = new Jugador[numMaxJugadores];
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
